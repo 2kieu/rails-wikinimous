0f366f11fbdcd00654b5daa828ab33b7c9e22092
Thu Kieu <t.kieu@live.de>
2024-01-23 19:22:09 +0100

    rails wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Book.genre,
+  )
+  article.save
+
+  # danach Terminal -> rails db:seed -> rails console -> Article.all
+
+end
